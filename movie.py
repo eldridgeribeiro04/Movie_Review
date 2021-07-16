db0f08671f00428f0be0bdd2c1ef45918e0a55dd
unknown <eldridgeriberio@gmail.com>
2021-07-16 15:42:56 +0100

    saveda model and made predicton on own data

--- movie.py
+++ movie.py
@@ -4,7 +4,7 @@ import numpy as np
 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 
 word_index = data.get_word_index()
 
@@ -24,11 +24,11 @@ test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_ind
 
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
-
 # print(decode_review(test_data[1]))
 
+"""
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.Embedding(88000, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
@@ -49,10 +49,37 @@ results = model.evaluate(test_data, test_labels)
 
 print(results)
 
-test_review = test_data[0]
-predict = model.predict([test_review])
-print("Review: ")
-print(decode_review(test_review))
-print("prediction: " + str(predict[0]))
-print("Actual: " + str(test_labels[0]))
-print(results)
+model.save("model.h5")"""
+
+
+def review_encode(s):
+    encoded = [1]
+
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+    return encoded
+
+
+model = keras.models.load_model("model.h5")
+
+
+with open("review.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        nline = line.replace(", ", "").replace(".", "").replace("(", "").replace(")", "").replace(":", "").replace("\"", "").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post", maxlen=250)
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
+
+# test_review = test_data[0]
+# predict = model.predict([test_review])
+# print("Review: ")
+# print(decode_review(test_review))
+# print("prediction: " + str(predict[0]))
+# print("Actual: " + str(test_labels[0]))
+# print(results)
